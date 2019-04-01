d5e9b6904e8af36151937006482327254d9af43a
Kyppy Ngaira Simani <kyppy.simani@andela.com>
2019-04-01 23:11:36 +0300

    #164829363  Completed 'profile' model fields.

--- authors/apps/profiles/models.py
+++ authors/apps/profiles/models.py
@@ -3,8 +3,18 @@ from authors import settings
 
 
 class Profile(models.Model):
-    """This class represents the model for user profiles"""
+    """This class represents the model for Author's Haven user profiles."""
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+    user_bio = models.CharField(max_length=300, help_text='Write a brief '
+                                'description about yourself.')
+    name = models.CharField(max_length=50, help_text='Enter your first '
+                            'and last names.')
+    number_of_followers = models.IntegerField(default=0)
+    number_of_followings = models.IntegerField(default=0)
+    total_articles = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{}".format(self.user.username)
