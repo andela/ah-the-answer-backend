4874f728e45f848ec6fad2b80191c7f71a589a5a
Kyppy Ngaira Simani <kyppy.simani@andela.com>
2019-04-01 23:27:58 +0300

    #164829363 Created initial unit test.

--- authors/apps/profiles/tests.py
+++ authors/apps/profiles/tests.py
@@ -1,2 +1,21 @@
 from django.test import TestCase
 from django.test import Client
+
+from .models import Profile
+from authentication.models import User
+
+
+class TestProfileModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="Bob",
+            email="demo@mail.com",
+            password="123"
+        )
+
+    def test_profile_user_relation(self):
+        u = self.user
+        profile = Profile(user=u, user_bio="Biography", name="Bobby Doe")
+        profile.save()
+        username = profile.user.username
+        self.assertEqual(username, "Bob")
