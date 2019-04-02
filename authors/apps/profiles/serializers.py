df829b4b136a0a7fc9e5167b4711905d8c39802b
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-04-02 11:49:40 +0300

    Feature(Profile Serializer): Implement Serialization for Profile model
    - Add Serializer class for profile
    - Add Serializer class for user's avatar
    - Create models.py file
    - Create tests.py file
    [#164829363]

---
+++ authors/apps/profiles/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework.serializers import ModelSerializer, ReadOnlyField
+from .models import Profile
+
+
+class ImageSerializer(ModelSerializer):
+
+    class Meta:
+        model = Record
+        fields = ('avatar',)
+
+    def update(self, instance, validated_data):
+        instance.image = validated_data.get('avatar', instance.image)
+        instance.save()
+
+        return instance
+
+
+class ProfileSerializer(ModelSerializer):
+
+    username = ReadOnlyField(source='get_username')
+    image_url = ReadOnlyField(source='cloudinary_image_url')
+
+    class Meta:
+        model = Profile
+        fields = ('username', 'user_bio',
+                  'name', 'email', 'total_articles',
+                  'number_of_following', 'number_of_followers', 'avatar')
+
+    def create(self, validated_data):
+        return Profile.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.user_bio = validated_data.get('user_bio', instance.user_bio)
+        instance.name = validated_data.get('name', instance.name)
+        instance.total_articles = validated_data.get('total_articles', instance.total_articles)
+        instance.number_of_following = validated_data.get('number_of_following', instance.number_of_following)
+        instance.number_of_followers = validated_data.get('number_of_followers', instance.number_of_followers)
+
+        instance.save()
+
+        return instance
+
