3a920abdd578e36f6200ff3e365fd1777dc68eba
ipaullly <ipaully22@gmail.com>
2019-04-02 14:42:08 +0300

    Create sendgrid function that sends password reset link to user email

---
+++ authors/apps/authentication/utilities.py
@@ -0,0 +1,22 @@
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+def dispatch_email(to_email, subject, message):
+    """
+    This method handles the sending of mails to users
+    """
+    sg = sendgrid.SendGridAPIClient(api_key=os.getenv("SENDGRID_API_KEY"))
+    from_email = Email(os.getenv("FROM_EMAIL"))
+    to_email = Email(to_email)
+    subject = subject
+    content = Content("text/plain", message)
+
+    try:
+        mail = Mail(from_email, subject, to_email, content)
+        response = sg.client.mail.send.post(request_body=mail.get())
+        if response.status_code is not 202:
+            return "check sender email and API key settings"
+        return "Email sent"
+    except Exception:
+        return "Error while sending mail to provided account"
