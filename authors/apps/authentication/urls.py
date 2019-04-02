0014e90d194aba5e64628b1e6e02e7901dd853ec
ipaullly <ipaully22@gmail.com>
2019-04-02 10:52:12 +0300

    [starts #164829361] add url for password reset

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import (
-    LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
+    LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView, PasswordResetAPIView
 )
 
 urlpatterns = [
     path('user/', UserRetrieveUpdateAPIView.as_view()),
     path('users/', RegistrationAPIView.as_view(), name="user-signup"),
     path('users/login/', LoginAPIView.as_view(), name="user-login"),
+    path('users/reset_password/', PasswordResetAPIView, name="password-reset")
 ]
