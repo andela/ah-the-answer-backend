e8a6d0a013e2c077be687e2af24f5132b804ed5b
ipaullly <ipaully22@gmail.com>
2019-04-03 11:20:59 +0300

    add url for PUT reset password view

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
-    LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView, PasswordResetAPIView
+    LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView, 
+    PasswordResetAPIView, SetUpdatedPasswordAPIView
 )
 
 urlpatterns = [
     path('user/', UserRetrieveUpdateAPIView.as_view()),
     path('users/', RegistrationAPIView.as_view(), name="user-signup"),
     path('users/login/', LoginAPIView.as_view(), name="user-login"),
-    path('users/reset_password/', PasswordResetAPIView.as_view(), name="password-reset")
+    path('users/reset_password/', PasswordResetAPIView.as_view(), name="password-reset"),
+    path('users/reset_password/<reset_token>', SetUpdatedPasswordAPIView.as_view(), name="set-updated-password")
 ]
