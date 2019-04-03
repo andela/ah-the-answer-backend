489113f3b7d0796fa530bc29db29e3acd042f002
ipaullly <ipaully22@gmail.com>
2019-04-03 10:10:24 +0300

    edit sendGrid function to return custom message

--- authors/apps/authentication/utilities.py
+++ authors/apps/authentication/utilities.py
@@ -2,13 +2,13 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-def dispatch_email(to_email, subject, message):
+def dispatch_email(user_email, subject, message):
     """
     This method handles the sending of mails to users
     """
     sg = sendgrid.SendGridAPIClient(apikey=os.getenv("SENDGRID_API_KEY"))
     from_email = Email(email=os.getenv("FROM_EMAIL"))
-    to_email = Email(email=to_email)
+    to_email = Email(email=user_email)
     subject = subject
     content = Content("text/plain", message)
 
@@ -17,6 +17,6 @@ def dispatch_email(to_email, subject, message):
         response = sg.client.mail.send.post(request_body=mail.get())
         if response.status_code is not 202:
             return "check sender email and API key settings"
-        return "Email sent"
+        return "Email sent to {}".format(user_email)
     except Exception:
         return "Error while sending mail to provided account"
