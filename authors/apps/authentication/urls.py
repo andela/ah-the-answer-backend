780d1f21774bd2920b392dce17eb69031f5e756d
ipaullly <ipaully22@gmail.com>
2019-04-03 08:06:17 +0300

    edit view in password reset URL

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('user/', UserRetrieveUpdateAPIView.as_view()),
     path('users/', RegistrationAPIView.as_view(), name="user-signup"),
     path('users/login/', LoginAPIView.as_view(), name="user-login"),
-    path('users/reset_password/', PasswordResetAPIView, name="password-reset")
+    path('users/reset_password/', PasswordResetAPIView.as_view(), name="password-reset")
 ]
