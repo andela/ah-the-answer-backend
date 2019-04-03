195ab7492acc8595d090c991dae9cd092b097063
kihuha <njihiadarius@gmail.com>
2019-04-03 14:06:51 +0300

    Update tests files

--- authors/apps/authentication/tests/test_email.py
+++ authors/apps/authentication/tests/test_email.py
@@ -7,34 +7,13 @@ from ..models import User
 
 class EmailTest(TestCase):
     def setUp(self):
-        self.client = test.APIClient()
-        self.user = User.objects.create(
-            email="test@mail.com",
-            username="test",
-            password="test1234"
-        )
+        pass
 
     def test_user_not_verified_on_create(self):
         pass
-        # self.assertFalse(self.user.is_verified)
 
     def test_email_sent(self):
         pass
-        # initial_count = len(mail.outbox)
-        # response = self.client.post(
-        #     reverse('authentication:user-signup'),
-        #     data={
-        #         "user": {
-        #             "email": "emailer@mail.com",
-        #             "username": "emailer",
-        #             "password": "emailer1234"
-        #         }
-        #     },
-        #     format="json"
-        # )
-
-        # self.assertNotEqual(initial_count, len(mail.outbox))
-        # self.assertTrue(initial_count < len(mail.outbox))
 
     def test_sent_email_content(self):
         pass
@@ -44,4 +23,3 @@ class EmailTest(TestCase):
 
     def tearDown(self):
         pass
-        # User.objects.get(id=self.user.id).delete()
--- authors/apps/authentication/tests/test_password_reset.py
+++ authors/apps/authentication/tests/test_password_reset.py
@@ -6,34 +6,13 @@ from ..models import User
 
 class PasswordResetTest(TestCase):
     def setUp(self):
-        self.client = test.APIClient()
-        self.user = User.objects.create(
-            email="test@mail.com",
-            username="test",
-            password="test1234"
-        )
+        pass
 
     def test_password_reset_request(self):
         pass
-        # response = self.client.post(
-        #     reverse('authentication:password-reset'),
-        #     data={
-        #         "email": "test@mail.com"
-        #     }
-        # )
-
-        # self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
 
     def test_password_reset_email_non_existent(self):
         pass
-        # response = self.client.post(
-        #     reverse('authentication:password-reset'),
-        #     data={
-        #         "email": "testerror@mail.com"
-        #     }
-        # )
-
-        # self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_password_reset_email_missing_request(self):
         pass
