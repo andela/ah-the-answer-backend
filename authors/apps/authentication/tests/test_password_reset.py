a85b5c79c64f2dda4de0e32de2b042a23eb6f18b
ipaullly <ipaully22@gmail.com>
2019-04-04 14:10:32 +0300

    resolve conflicts in test module and requirements.txt

--- authors/apps/authentication/tests/test_password_reset.py
+++ authors/apps/authentication/tests/test_password_reset.py
@@ -2,9 +2,6 @@ import json
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import test, status
-#from django.urls import reverse
-#from rest_framework.test import APITestCase, APIClient
-#from rest_framework.views import status
 from ..models import User, ResetPasswordToken
 
 
