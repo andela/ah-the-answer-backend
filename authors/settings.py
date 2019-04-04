597fc8c6e67004fab377a517beb4b340b92ed41a
Kyppy Simani <kyppy.simani@andela.com>
2019-04-04 19:20:00 +0300

    Fixed migration error

--- authors/settings.py
+++ authors/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
 
 
 cloudinary.config(
-  cloud_name=config("CLOUDINARY_NAME"),
-  api_key=config("CLOUDINARY_API_KEY"),
-  api_secret=config("CLOUDINARY_API_SECRET")
+  cloud_name="dvrmobxpm",
+  api_key="656296195734553",
+  api_secret="JjWji3WYcaaZN3J5J6k0x0As0vU"
 )
