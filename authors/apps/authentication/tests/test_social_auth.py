043a1e7a0823ddb132e412466d565dc1a6bd1e85
Joseph Kirega <kirega@Josephs-MacBook-Pro.local>
2019-04-04 12:53:29 +0300

    [ft #164829362] Updates tests

--- authors/apps/authentication/tests/test_social_auth.py
+++ authors/apps/authentication/tests/test_social_auth.py
@@ -1,35 +1,38 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework import test, status
 
 
 class SocialAuthTest(TestCase):
+    def setUp(self):
+        self.client = test.APIClient()
 
     # FACEBOOK
-    def test_user_signup_success(self):
-        pass
+    def test_user_signup_page_redirects_to_facebook(self):
+        """
+            Ensures that the redirects are found, basically tests
+            that the configurations are functional
+        """
+        res = self.client.get(
+            reverse('social:begin', kwargs={'backend': 'facebook'}))
+        self.assertEqual(res.status_code, status.HTTP_302_FOUND) 
 
-    def test_user_signup_invalid_fields(self):
-        pass
-
-    def test_empty_fields(self):
-        pass
-
-    # TWITTER
-    def test_user_signup_success(self):
-        pass
-
-    def test_user_signup_invalid_fields(self):
-        pass
-
-    def test_empty_fields(self):
-        pass
+    # Github
+    def test_user_signup_page_redirects_to_github(self):
+        """
+            Ensures that the redirects are found, basically tests
+            that the configurations are functional
+        """
+        res = self.client.get(
+            reverse('social:begin', kwargs={'backend': 'github'}))
+        self.assertEqual(res.status_code, status.HTTP_302_FOUND)
 
     # GOOGLE
-    def test_user_signup_success(self):
-        pass
-
-    def test_user_signup_invalid_fields(self):
-        pass
-
-    def test_empty_fields(self):
-        pass
+    def test_user_signup_page_redirects_to_github(self):
+        """
+            Ensures that the redirects are found, basically tests
+            that the configurations are functional
+        """
+        res = self.client.get(
+            reverse('social:begin', kwargs={'backend': 'google-oauth2'}))
+        self.assertEqual(res.status_code, status.HTTP_302_FOUND)
