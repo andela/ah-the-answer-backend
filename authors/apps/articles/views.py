2a30107aeee70849681d2dde100c50e55b26e622
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-04-07 10:20:30 +0300

    Removes code repetition [#164829364]

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -10,6 +10,17 @@ from authors.apps.authentication.models import User
 import cloudinary
 
 
+def find_article(slug):
+    """Method to check if an article exists"""
+    try:
+        return Article.objects.get(slug=slug)
+    except Article.DoesNotExist:
+        APIException.status_code = 404
+        raise APIException({
+            "message": "The article requested does not exist"
+        })
+
+
 class ArticleView(APIView):
     """Class that contains the method that retrieves all articles and creates an article"""
     permission_classes = (IsAuthenticated | ReadOnly,)
@@ -29,8 +40,7 @@ class ArticleView(APIView):
         if serializer.is_valid(raise_exception=True):
             article_saved = serializer.save(author=self.request.user)
 
-        return Response(
-            {
+        return Response({
                 "success": "Article '{}' created successfully".format(article_saved.title),
                 "article": serializer.data
             }, status=201)
@@ -43,25 +53,16 @@ class RetrieveArticleView(APIView):
     def is_owner(self, current_user_id, article_author_id):
         if article_author_id == current_user_id:
             return True
-        return False
 
     def get(self, request, slug):
         """Method to get a specific article"""
-        try:
-            article = Article.objects.get(slug=slug)
-            serializer = ArticleSerializer(article, many=False)
-            return Response({"article": serializer.data})
-        except Article.DoesNotExist:
-            return Response(
-                {"message": "The article requested does not exist"}, status=404)
+        article = find_article(slug)
+        serializer = ArticleSerializer(article, many=False)
+        return Response({"article": serializer.data})
 
     def put(self, request, slug):
         """Method to update a specific article"""
-        try:
-            saved_article = Article.objects.get(slug=slug)
-        except Article.DoesNotExist:
-            return Response(
-                {"message": "The article requested does not exist"}, status=404)
+        saved_article = find_article(slug)
 
         data = request.data.get('article')
         serializer = ArticleSerializer(
@@ -78,11 +79,7 @@ class RetrieveArticleView(APIView):
 
     def delete(self, request, slug):
         """Method to delete a specific article"""
-        try:
-            article = Article.objects.get(slug=slug)
-        except Article.DoesNotExist:
-            return Response(
-                {"message": "The article requested does not exist"}, status=404)
+        article = find_article(slug)
 
         if self.is_owner(article.author.id, request.user.id) is True:
             article.delete()
@@ -98,12 +95,7 @@ class ArticleImageView(APIView):
 
     def post(self, request, slug):
         """Method to upload an image"""
-
-        try:
-            article = Article.objects.get(slug=slug)
-        except Article.DoesNotExist:
-            return Response(
-                {"message": "The article requested does not exist"}, status=404)
+        article = find_article(slug)
 
         if request.FILES:
             try:
@@ -129,6 +121,7 @@ class ArticleImageView(APIView):
 
     def get(self, request, slug):
         """Method to get all images of an article"""
+        find_article(slug)
         images = ArticleImage.objects.select_related(
             'article').filter(article__slug=slug)
         serializer = ArticleImageSerializer(images, many=True)
