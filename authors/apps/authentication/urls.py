381aaee3c2a8c5397788b345f285754bea58a5e6
kirega <joseph.mutiga@gmail.com>
2019-04-08 01:53:25 +0300

    [ft #164829362] Add views for social login

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -4,7 +4,10 @@ from .views import (
     RegistrationAPIView,
     UserRetrieveUpdateAPIView,
     PasswordResetAPIView,
-    SetUpdatedPasswordAPIView
+    SetUpdatedPasswordAPIView,
+    GoogleAuthView,
+    FacebookAuthAPIView,
+    TwitterAuthAPIView
 )
 
 urlpatterns = [
@@ -12,5 +15,9 @@ urlpatterns = [
     path('users/', RegistrationAPIView.as_view(), name="user-signup"),
     path('users/login/', LoginAPIView.as_view(), name="user-login"),
     path('users/reset_password/', PasswordResetAPIView.as_view(), name="password-reset"),
-    path('users/reset_password/<reset_token>/', SetUpdatedPasswordAPIView.as_view(), name="set-updated-password")
+    path('users/reset_password/<reset_token>/', SetUpdatedPasswordAPIView.as_view(), name="set-updated-password"),
+    path('users/google/', GoogleAuthView.as_view(), name="google"),
+    path('users/facebook/', FacebookAuthAPIView.as_view(), name="facebook"),
+    path('users/twitter/', TwitterAuthAPIView.as_view(), name="twitter"),
+
 ]
