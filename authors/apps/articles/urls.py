165288950d2bb70a3a6a70a2258aeb41b90f3a2d
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-04-09 15:37:03 +0300

    Feature(Article Review): Add utils and urls files for reviews endpoint
    - Add permission validation functions in the utils.py file
    - Add reviews endpoint in urls.py file.
    
    [#164829365]

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ArticleView, RetrieveArticleView, ArticleImageView
+from .views import ArticleView, RetrieveArticleView, ArticleImageView, ReviewView
 
 
 app_name = "articles"
@@ -9,5 +9,7 @@ app_name = "articles"
 urlpatterns = [
     path('articles/', ArticleView.as_view(), name="create-list"),
     path('articles/<slug>/', RetrieveArticleView.as_view(), name="details"),
-    path('articles/<slug>/image/', ArticleImageView.as_view(), name="add-image")
+    path('articles/<slug>/image/', ArticleImageView.as_view(), name="add-image"),
+    path('articles/<slug>/reviews/', ReviewView.as_view(), name="review")
+
 ]
--- authors/apps/articles/utils.py
+++ authors/apps/articles/utils.py
@@ -8,7 +8,22 @@ def generate_slug(article_title):
     unique_slug = article_title + ' ' + unique_tag
     return slugify(unique_slug)
 
+
 def get_readtime(article_body):
     """Method to calculate the read time of an article."""
     result = readtime.of_text(article_body)
     return result.text
+
+
+def is_article_owner(author, reviewer):
+    if author == reviewer:
+        return True
+
+
+def has_reviewed(article, reviewer):
+    from .models import ReviewsModel
+
+    count = ReviewsModel.objects.filter(article=article,
+                                        reviewed_by=reviewer).count()
+    if count > 0:
+        return True
