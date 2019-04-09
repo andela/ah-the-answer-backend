c6235d4c3c4e51131c3fd224d29376ce1dbcfbe5
Kyppy Simani <kyppy.simani@andela.com>
2019-04-09 14:07:45 +0300

    [164829369] Linted code of completed feature for review.

--- authors/apps/follow/serializers.py
+++ authors/apps/follow/serializers.py
@@ -3,13 +3,8 @@ from .models import Follows
 
 
 class FollowersSerializer(serializers.Serializer):
-    #followed_user = serializers.CharField(max_length=100)
     following_user = serializers.CharField(max_length=100)
-    #follower_id = serializers.IntegerField()
 
 
 class FollowingSerializer(serializers.Serializer):
     followed_user = serializers.CharField(max_length=100)
-    
-
-    
