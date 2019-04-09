733ac491c90fa79361aa881a58f2ea54255e0465
kihuha <njihiadarius@gmail.com>
2019-04-09 10:52:27 +0300

    [Feature #164829366] comments CRUD

--- authors/apps/articles/utils.py
+++ authors/apps/articles/utils.py
@@ -1,8 +1,14 @@
 from django.utils.text import slugify
 import secrets
+import readtime
 
 
 def generate_slug(article_title):
     unique_tag = str(secrets.token_hex(6))
     unique_slug = article_title + ' ' + unique_tag
     return slugify(unique_slug)
+
+def get_readtime(article_body):
+    """Method to calculate the read time of an article."""
+    result = readtime.of_text(article_body)
+    return result.text
