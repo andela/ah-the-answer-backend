a77134e392c13cbdee3df13185210db23531b420
ipaullly <ipaully22@gmail.com>
2019-04-10 23:05:33 +0300

    edit ArticleSerilizer to include likes/dislikes count

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -10,6 +10,8 @@ class ArticleSerializer(serializers.ModelSerializer):
     title = serializers.CharField(max_length=100)
     description = serializers.CharField(max_length=128)
     body = serializers.CharField()
+    like_count = serializers.SerializerMethodField()
+    dislike_count = serializers.SerializerMethodField()
 
     def create(self, validated_data):
         return Article.objects.create(**validated_data)
@@ -22,11 +24,20 @@ class ArticleSerializer(serializers.ModelSerializer):
         instance.is_published = validated_data.get('is_published', instance.is_published)
         instance.save()
         return instance
+    
+    def get_like_count(self, obj):
+        """method returns number of likes for an article"""
+        return obj.liked.filter(likes=1).count()
+
+    def get_dislike_count(self, obj):
+        """method returns the sum of dislikes for an article"""
+        return obj.liked.filter(dislikes=-1).count()
 
     class Meta:
         model = Article
         fields = ('id', 'title', 'body', 'description', 'is_published',
-                  'date_created', 'date_modified', 'slug', 'read_time', 'author')
+                  'date_created', 'date_modified', 'slug', 'read_time', 'author',
+                  'like_count', 'dislike_count')
         read_only_fields = ('date_created', 'date_modified', 'slug', 'read_time', 'author')
 
 
