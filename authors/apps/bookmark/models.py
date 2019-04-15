11623a7fe3998e6d4696174249cf8b9be5fbe517
Kyppy Simani <kyppy.simani@andela.com>
2019-04-15 12:10:00 +0300

    Create 'bookmark' model and initialize migrations

--- authors/apps/bookmark/models.py
+++ authors/apps/bookmark/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from authors import settings
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    article_title = modles.Charfield(max_length=100)
+    article_id = models.IntegerField()
+    user = models.ManyToManyField(
+           settings.AUTH_USER_MODEL,
+           on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        """Defines a human readable name for a
+        bookmark database query object."""
+
+        return "{}".format(self.article_title)
+
