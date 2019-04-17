960575d504bc37e39c9111670fef55bccf60b940
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-04-17 20:08:11 +0300

    test migrations folder error

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 echo "run migrations start"
-
+python manage.py migrate --fake authentication zero
 python manage.py makemigrations authentication comments follow profile
 python manage.py migrate
 
