84232b194c8466dc44b48feed12da65d1fd79cc6
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-04-17 14:53:03 +0300

    Feature(Comment History Tracker): Implement comment history serializer
    - Add custom 'CommentHistoryField' that inherits from 'ListField' class
    - Add a read only serializer field for comment history retrieval
    
    [#164829381]

--- authors/apps/comments/serializers.py
+++ authors/apps/comments/serializers.py
@@ -5,11 +5,19 @@ from authors.apps.profiles.serializers import ProfileSerializer
 from authors.apps.articles.serializers import ArticleSerializer
 
 
+class CommentHistoryField(serializers.ListField):
+    child = serializers.DictField()
+
+    def to_representation(self, data):
+        return super().to_representation(data.values())
+
+
 class CommentSerializer(serializers.ModelSerializer):
     body = serializers.CharField()
     author = UserSerializer(read_only=True)
+    comment_history = CommentHistoryField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ['id', 'createdAt', 'updatedAt', 'body', 'author']
-        read_only = ['id', 'createdAt', 'updatedAt', 'author']
+        fields = ['id', 'createdAt', 'updatedAt', 'body', 'author', 'comment_history']
+        read_only = ['id', 'createdAt', 'updatedAt', 'author', 'comment_history']
