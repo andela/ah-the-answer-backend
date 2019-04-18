9ddb0deb31f7cde8423fd26ed122b7ac507003f2
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-04-18 11:03:58 +0300

    Add migrations for all apps

--- release.sh
+++ release.sh
@@ -3,6 +3,14 @@ python manage.py makemigrations authentication
 python manage.py migrate authentication
 python manage.py makemigrations auth
 python manage.py migrate auth
+python manage.py makemigrations profiles
+python manage.py migrate profiles
+python manage.py makemigrations follow
+python manage.py migrate follow
+python manage.py makemigrations articles
+python manage.py migrate articles
+python manage.py makemigrations comments
+python manage.py migrate comments
 python manage.py migrate
 python manage.py syncdb
 echo "Done"
