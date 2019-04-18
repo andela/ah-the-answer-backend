f8a2bd60450f6a9591c104ec85212d521462f547
Larry <larry@Larrys-MacBook-Pro.local>
2019-04-18 07:14:12 +0300

    [feature #164829374] Add previous and next urls

--- authors/apps/articles/filters.py
+++ authors/apps/articles/filters.py
@@ -1,6 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Article
-from django.core.paginator import Paginator
 
 
 class ArticleFilter(DjangoFilterBackend):
@@ -9,14 +8,4 @@ class ArticleFilter(DjangoFilterBackend):
         author = request.query_params.get('author')
         if author:
             return filter_class(request.query_params, queryset=Article.objects.all().filter(author__username=author), request=request).qs
-
-        # pagination
-        page = request.GET.get('page')
-        article_list = Article.objects.all()
-        paginator = Paginator(article_list, 10)
-        if page:
-            page_articles = paginator.get_page(page)
-            return page_articles
-
-        articles = paginator.get_page(1)
-        return articles
+        return filter_class(request.query_params, queryset=queryset, request=request).qs
