f332c778d6e37c7d4f4a11ab8d2684a67ed85e7c
Kyppy Simani <kyppy.simani@andela.com>
2019-04-23 09:13:24 +0300

    Created report model

--- authors/apps/report/models.py
+++ authors/apps/report/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from authors import settings
+from authors.apps.articles.models import Article
 
-# Create your models here.
+
+class Report(model.Model):
+    """
+    This class represents the model for Author's Haven that records user
+    reports on articles.
+    """
+    VIOLATION_CHOICES = (
+        ('Hate Speech', 'Hate Speech'),
+        ('Harrassment', 'Harassment'),
+        ('Privacy and Reputation', 'Privacy and Reputation'),
+        ('Spam', 'Spam'),
+        ('Bot Account', 'Bot Account'),
+        ('Deceptive Conduct', 'Deceptive Conduct'),
+        ('Graphic Content', 'Graphic Content'),
+        ('Exploitation of Minors', 'Exploitation of Minors'),
+        ('Promotion of Self-harm', 'Promotion of Self-harm'),
+        ('Other', 'Other')
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE
+    )
+    reporter = models.EmailField
+    createdAt = models.DateTimeField(auto_now_add=True)
+    resolvedAt = models.DateField(default=None)
+    violation = models.CharField(choices=VIOLATION_CHOICES)
+    reportDetails = models.TextField(default=None)
+    isResolved = models.BooleanField(default=False)
+    adminNote = models.TextField(default=None)
+
+
+
+
+
