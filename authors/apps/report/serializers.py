527d4666b7210592058059546734fd89c2b9fb61
Kyppy Simani <kyppy.simani@andela.com>
2019-04-23 21:31:38 +0300

    Updated serializer to perform update function

--- authors/apps/report/serializers.py
+++ authors/apps/report/serializers.py
@@ -15,5 +15,29 @@ class ReportSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Report
-        fields = ['article_id', 'reporter', 'violation', 'reportDetails',
+        fields = ['reporter', 'violation', 'reportDetails',
                   'isResolved', 'adminNote']
+    
+    def create(self, validated_data):
+        """
+        Creates new report record after validation.
+        """
+
+        return Report.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Passes in an instance of the report to be updated.
+        Update occurs if new values are provided.
+        Otheriwse instance values remain the same.
+        """
+
+        instance.resolvedAt = validated_data.get('resolvedAt',
+                                                 instance.resolvedAt)
+        instance.isResolved = validated_data.get('isResolved',
+                                                 instance.isResolved)
+        instance.adminNote = validated_data.get('adminNote',
+                                                instance.adminNote)
+        instance.save()
+
+        return instance
