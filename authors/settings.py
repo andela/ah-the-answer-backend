f914e3e139bf500186cc8f869d56de638e10bdca
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-04-24 20:42:15 +0300

    add changes to effect static file recognition on heroku

--- authors/settings.py
+++ authors/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, '/staticfiles/')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ORIGIN_WHITELIST = (
