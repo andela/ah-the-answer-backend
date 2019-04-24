2135fe1a015f6d513b4cf7c36a66e673aaa8411f
Kyppy Simani <kyppy.simani@andela.com>
2019-04-24 12:20:32 +0300

    Removed redundancies

--- authors/apps/report/serializers.py
+++ authors/apps/report/serializers.py
@@ -17,27 +17,3 @@ class ReportSerializer(serializers.ModelSerializer):
         model = Report
         fields = ['id', 'reporter', 'violation', 'reportDetails',
                   'isResolved', 'adminNote']
-    
-    def create(self, validated_data):
-        """
-        Creates new report record after validation.
-        """
-
-        return Report.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Passes in an instance of the report to be updated.
-        Update occurs if new values are provided.
-        Otheriwse instance values remain the same.
-        """
-
-        instance.resolvedAt = validated_data.get('resolvedAt',
-                                                 instance.resolvedAt)
-        instance.isResolved = validated_data.get('isResolved',
-                                                 instance.isResolved)
-        instance.adminNote = validated_data.get('adminNote',
-                                                instance.adminNote)
-        instance.save()
-
-        return instance
