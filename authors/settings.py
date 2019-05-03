b1cc0a297c181a2460381ca6bc955669060ea846
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-03 17:59:23 +0300

    Moves CorsMiddleware to the top

--- authors/settings.py
+++ authors/settings.py
@@ -68,9 +68,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
